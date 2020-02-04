3800e09305f2fbc5241bfab573e0c52849fd832d
Nicolas Marshall <marshall.nicolas@gmail.com>
2020-02-04 11:17:04 -0400

    rust deps, tests

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,5 +1,16 @@
+use ed25519_dalek::Keypair;
+use ed25519_dalek::Signature;
+use rand::rngs::OsRng;
+
+pub fn yo() {
+    let mut csprng = OsRng {};
+    let keypair: Keypair = Keypair::generate(&mut csprng);
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
