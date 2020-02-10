c7422e18238a74d02f46fc8f8d17b2c38f51785a
Nicolas Marshall <marshall.nicolas@gmail.com>
2020-02-10 15:06:18 -0400

    rs

--- makefile
+++ makefile
@@ -1,3 +1,14 @@
 test:
 	cargo test -- --nocapture
 	# yarn test
+build-w:
+	# cargo build --target wasm32-unknown-unknown --release
+	# wasm-gc target/wasm32-unknown-unknown/release/sig_lib.wasm -o .cache/sig_lib.min.wasm
+	# wasm2js .cache/sig_lib.min.wasm -o .cache/sig_lib.min.js
+	wasm-pack build rust --target no-modules --out-dir ../.cache/ed25519-sigs/dist/no-modules/
+	wasm-pack build rust --target browser --out-dir ../.cache/ed25519-sigs/dist/browser/
+	wasm-pack build rust --target nodejs --out-dir ../.cache/ed25519-sigs/dist/node/
+w.test:
+	cd rust && wasm-pack test --chrome
+ts.test:
+	npx jest typescript/deps.spec.ts
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,73 +1,104 @@
-// pub fn yo() {}
-// use rand::rand_chacha::ChaChaRng;
+#![allow(non_camel_case_types)]
+use crypto::ed25519;
+
+#[no_mangle]
+pub extern "C" fn add(x: u32, y: u32) -> u32 {
+    x + y
+}
+
+#[no_mangle]
+pub extern "C" fn keypair(seed: &[u8]) -> ([u8; 64], [u8; 32]) {
+    ed25519::keypair(seed)
+}
 
 #[cfg(test)]
 mod tests {
-    #![allow(non_camel_case_types)]
-    // use super::*;
-    use ed25519_dalek::{
-        Keypair, PublicKey, Signature, KEYPAIR_LENGTH, PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH,
-    };
-    // use rand::rngs::adapter::ReadRng;
-    use rand::rngs::{OsRng, StdRng};
-    use rand::SeedableRng;
-    use std::convert::TryInto;
+    use super::*;
+    // use ed25519_dalek::{
+    //     Keypair, PublicKey, Signature, KEYPAIR_LENGTH, PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH,
+    // };
+    // use rand::rngs::StdRng;
+    // use rand::SeedableRng;
+    // use rand_chacha::ChaCha20Rng;
+    // use rand_core::RngCore;
+    // use std::convert::TryInto;
 
-    fn seed_rng() -> StdRng {
-        // let seed: &[u8] = &[1, 2, 3, 4,5,6,7,8,9,10];
-        let seed_str: [u8; 32] = "correct horse battery staple pad"
-            .as_bytes()
-            .try_into()
-            .expect("slice with incorrect length");
-        dbg!(seed_str);
-        let rng: StdRng = StdRng::from_seed(seed_str);
-        // let mut rng = StdRng::from_seed(SeedableRng::seed_from_u64(&mut 1))
-        rng
-    }
+    // fn seed_rng() -> ChaCha20Rng {
+    //     // let seed: &[u8] = &[1, 2, 3, 4,5,6,7,8,9,10];
+    //     let seed_str: [u8; 32] = "correct horse battery staple pad"
+    //         .as_bytes()
+    //         .try_into()
+    //         .expect("slice with incorrect length");
+    //     let rng = ChaCha20Rng::from_seed(seed_str);
+    //     // let rng = Rng(ChaCha::new(&seed, &[0u8; 8]));
+    //     // let mut rng = StdRng::from_seed(SeedableRng::seed_from_u64(&mut 1))
+    //     rng
+    // }
 
-    #[test]
-    fn signs_and_verifies() {
-        let mut csprng = OsRng {};
-        let keypair: Keypair = Keypair::generate(&mut csprng);
+    // #[test]
+    // fn fill_bytes_seedrng() {
+    //     let mut rng = seed_rng();
+    //     let mut res = [0u8; 32];
+    //     rng.fill_bytes(&mut res);
 
-        let message: &[u8] = b"This is a test of the tsunami alert system.";
-        let signature: Signature = keypair.sign(message);
-        assert!(keypair.verify(message, &signature).is_ok());
+    //     // dbg!(res);
+    // }
 
-        let public_key: PublicKey = keypair.public;
-        assert!(public_key.verify(message, &signature).is_ok());
-    }
-    #[test]
-    fn ser_deser() -> Result<(), bErr> {
-        let mut csprng = OsRng {};
-        let keypair: Keypair = Keypair::generate(&mut csprng);
+    // #[test]
+    // fn scrypt_js() {
+    //     let default_scrypt_params = crypto::scrypt::ScryptParams::new(14, 8, 1);
+    //     let mut out = [0u8; 32];
+    //     let pass = "correct horse battery staple".as_bytes();
+    //     let salt = [0u8; 1];
 
-        let keypair_bytes: [u8; KEYPAIR_LENGTH] = keypair.to_bytes();
-        assert_eq!(keypair_bytes.len(), 64);
+    //     crypto::scrypt::scrypt(pass, &salt, &default_scrypt_params, &mut out);
 
-        let got_keypair = Keypair::from_bytes(&keypair_bytes)?;
-        assert_eq!(got_keypair.public, keypair.public);
-        assert_eq!(got_keypair.secret.to_bytes(), keypair.secret.to_bytes());
+    //     dbg!(out);
+    // }
 
-        Ok(())
-    }
+    // #[test]
+    // fn signs_and_verifies() {
+    //     let mut rng = seed_rng();
+    //     let keypair: Keypair = Keypair::generate(&mut rng);
 
-    #[test]
-    fn key_sigs_length() {
-        assert_eq!(PUBLIC_KEY_LENGTH, 32);
-        assert_eq!(SECRET_KEY_LENGTH, 32);
+    //     let message: &[u8] = b"This is a test of the tsunami alert system.";
+    //     let signature: Signature = keypair.sign(message);
+    //     assert!(keypair.verify(message, &signature).is_ok());
 
-        let keypair = new_keypair();
-        assert_eq!(keypair.public.to_bytes().len(), 32);
-        assert_eq!(keypair.secret.to_bytes().len(), 32);
+    //     let public_key: PublicKey = keypair.public;
+    //     assert!(public_key.verify(message, &signature).is_ok());
+    // }
+    // #[test]
+    // fn ser_deser() -> Result<(), bErr> {
+    //     let mut rng = seed_rng();
+    //     let keypair: Keypair = Keypair::generate(&mut rng);
 
-        let message: &[u8] = b"This is a test of the tsunami alert system.";
-        let sig: Signature = keypair.sign(message);
-        assert!(keypair.verify(message, &sig).is_ok());
-        assert_eq!(sig.to_bytes().len(), 64);
+    //     let keypair_bytes: [u8; KEYPAIR_LENGTH] = keypair.to_bytes();
+    //     assert_eq!(keypair_bytes.len(), 64);
 
-        // dbg!(kp);
-    }
+    //     let got_keypair = Keypair::from_bytes(&keypair_bytes)?;
+    //     assert_eq!(got_keypair.public, keypair.public);
+    //     assert_eq!(got_keypair.secret.to_bytes(), keypair.secret.to_bytes());
+
+    //     Ok(())
+    // }
+
+    // #[test]
+    // fn key_sigs_length() {
+    //     assert_eq!(PUBLIC_KEY_LENGTH, 32);
+    //     assert_eq!(SECRET_KEY_LENGTH, 32);
+
+    //     let keypair = new_keypair();
+    //     assert_eq!(keypair.public.to_bytes().len(), 32);
+    //     assert_eq!(keypair.secret.to_bytes().len(), 32);
+
+    //     let message: &[u8] = b"This is a test of the tsunami alert system.";
+    //     let sig: Signature = keypair.sign(message);
+    //     assert!(keypair.verify(message, &sig).is_ok());
+    //     assert_eq!(sig.to_bytes().len(), 64);
+
+    //     // dbg!(kp);
+    // }
 
     //
 
@@ -76,14 +107,14 @@ mod tests {
 
     type bErr = Box<dyn std::error::Error>;
 
-    fn new_keypair() -> Keypair {
-        // let data = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        // let mut rng = ReadRng::new(&data[..]);
-        // println!("{:x}", rng.gen::<u32>());
+    // fn new_keypair() -> Keypair {
+    //     // let data = vec![1, 2, 3, 4, 5, 6, 7, 8];
+    //     // let mut rng = ReadRng::new(&data[..]);
+    //     // println!("{:x}", rng.gen::<u32>());
 
-        let mut rng = seed_rng();
-        let keypair: Keypair = Keypair::generate(&mut rng);
-        dbg!(keypair.public);
-        keypair
-    }
+    //     let mut rng = seed_rng();
+    //     let keypair: Keypair = Keypair::generate(&mut rng);
+    //     // dbg!(keypair.public);
+    //     keypair
+    // }
 }
