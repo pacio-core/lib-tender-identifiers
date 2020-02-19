9c2661c469abac96dada492b5955835336c36ce1
Nicolas Marshall <marshall.nicolas@gmail.com>
2020-02-19 14:52:43 -0400

    re-adapt exported functions

--- rust/src/ts_fns.rs
+++ rust/src/ts_fns.rs
@@ -1,28 +1,40 @@
 use wasm_bindgen::prelude::*;
 //
 // use bip39::{Language, Mnemonic, MnemonicType, Seed};
-use blake2::{Blake2b, Digest};
-use crypto::ed25519;
+// use blake2::{Blake2b, Digest};
+
+// use crypto::ed25519;
+use super::{KeyPair, PublicKey, SeedPhrase, Signature};
 use js_sys::Uint8Array;
 
+// #[wasm_bindgen]
+// pub extern "C" fn gen_pubKey(seed: &[u8]) -> Uint8Array {
+//     let (_, pubKey) = ed25519::keypair(&seed);
+//     unsafe { Uint8Array::view(&pubKey) }
+// }
+// #[wasm_bindgen]
+// pub extern "C" fn gen_privKey(seed: &[u8]) -> Uint8Array {
+//     let (privKey, _) = ed25519::keypair(&seed);
+//     unsafe { Uint8Array::view(&privKey) }
+// }
 #[wasm_bindgen]
-pub extern "C" fn gen_pubKey(seed: &[u8]) -> Uint8Array {
-    let (_, pubKey) = ed25519::keypair(&seed);
-    unsafe { Uint8Array::view(&pubKey) }
+pub extern "C" fn gen_keypair(phrase: &str) -> Uint8Array {
+    let kp = KeyPair::from_phrase(phrase);
+    unsafe { Uint8Array::view(&kp.to_bytes()) }
 }
 #[wasm_bindgen]
-pub extern "C" fn gen_privKey(seed: &[u8]) -> Uint8Array {
-    let (privKey, _) = ed25519::keypair(&seed);
-    unsafe { Uint8Array::view(&privKey) }
+pub extern "C" fn sign(message: &[u8], keypair_bytes: &[u8]) -> Uint8Array {
+    let kp = KeyPair::from_bytes(keypair_bytes);
+    let sig_bytes = kp.sign(&message);
+    unsafe { Uint8Array::view(&sig_bytes) }
 }
 #[wasm_bindgen]
-pub extern "C" fn sign(message: &[u8], privKey: &[u8]) -> Uint8Array {
-    let sig = ed25519::signature(message, privKey);
-    unsafe { Uint8Array::view(&sig) }
-}
-#[wasm_bindgen]
-pub extern "C" fn verify(message: &[u8], pubKey: &[u8], signature: &[u8]) -> bool {
-    ed25519::verify(message, pubKey, signature)
+pub extern "C" fn verify(message: &[u8], pubKey_bytes: &[u8], sig_bytes: &[u8]) -> bool {
+    console_error_panic_hook::set_once();
+
+    let pubKey = PublicKey::from_bytes(pubKey_bytes).unwrap();
+    let sig = Signature::from_bytes(&sig_bytes).unwrap();
+    pubKey.verify(&message, &sig).is_ok()
 }
 
 // #[wasm_bindgen]
@@ -35,11 +47,7 @@ pub extern "C" fn verify(message: &[u8], pubKey: &[u8], signature: &[u8]) -> boo
 pub extern "C" fn seed_from_phrase(phrase: String) -> Uint8Array {
     console_error_panic_hook::set_once();
 
-    // let mnemo = Mnemonic::from_phrase(phrase, Language::English).unwrap();
-    // let mut hasher = Blake2b::new(64);
-    // let bytes = phrase.as_bytes();
-    let seed = Blake2b::digest(&phrase.as_bytes());
+    let seed = SeedPhrase::from_str(&phrase).into_seed();
 
-    // let seed = Seed::new(&phrase, "");
     unsafe { Uint8Array::view(&seed) }
 }
