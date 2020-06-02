47cfd0235aab2e4c7eb80574d3828215597c0e7e
Nicolas Marshall <marshall.nicolas@gmail.com>
2020-06-02 18:15:07 +0100

    keypair: to_, from_ str

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,13 +5,14 @@ use blake2::{Blake2b, Digest};
 use ed25519_dalek::{self as ed, SecretKey};
 
 pub use ed::{PublicKey, Signature};
-// mod bindings;
+mod errors;
+use errors::Error as EdErr;
 
 #[derive(Debug)]
 pub struct KeyPair(ed::Keypair);
 impl KeyPair {
     pub fn from_phrase(phrase: &str) -> Self {
-        let seed = SeedPhrase::from_str(phrase).into_seed();
+        let seed = SeedPhrase::from_str(phrase).to_seed();
         let secret = SecretKey::from_bytes(&seed).unwrap();
         let public: PublicKey = (&secret).into();
 
@@ -40,9 +41,17 @@ impl KeyPair {
     pub fn to_bytes(&self) -> [u8; 64] {
         self.0.to_bytes()
     }
-    pub fn from_bytes(bytes: &[u8]) -> Self {
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, EdErr> {
         assert_eq!(bytes.len(), 64);
-        Self(ed::Keypair::from_bytes(bytes).unwrap())
+        Ok(Self(ed::Keypair::from_bytes(bytes)?))
+    }
+
+    pub fn to_str(&self) -> String {
+        base64::encode(&self.to_bytes().to_vec())
+    }
+    pub fn from_str(token: &str) -> Result<Self, EdErr> {
+        let bytes = base64::decode(&token)?;
+        Ok(Self::from_bytes(&bytes)?)
     }
 }
 
@@ -52,16 +61,18 @@ impl SeedPhrase {
         let mnemonic = Mnemonic::new(MnemonicType::Words12, Language::English);
         Self(mnemonic.phrase().to_owned())
     }
+
+    pub fn to_seed(&self) -> [u8; 32] {
+        let mut arr: [u8; 32] = Default::default();
+        arr.copy_from_slice(&Blake2b::digest(self.0.as_bytes())[..32]);
+        arr
+    }
+
     pub fn from_str(s: &str) -> Self {
         return Self(s.to_owned());
     }
     pub fn to_str(&self) -> String {
         self.0.to_owned()
-    }
-    pub fn into_seed(&self) -> [u8; 32] {
-        let mut arr: [u8; 32] = Default::default();
-        arr.copy_from_slice(&Blake2b::digest(self.0.as_bytes())[..32]);
-        arr
     }
 }
 
@@ -135,7 +146,7 @@ mod tests {
         let ser = kp.to_bytes();
         assert_eq!(ser.len(), 64);
 
-        let deser = KeyPair::from_bytes(&ser);
+        let deser = KeyPair::from_bytes(&ser)?;
         assert_eq!(deser.0.public.to_bytes(), kp.0.public.to_bytes());
         assert_eq!(deser.0.secret.to_bytes(), kp.0.secret.to_bytes());
 
